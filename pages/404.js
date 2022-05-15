f79d13041f74c89657d6582c52e0740329d92ab1
andres <echeverry11298@gmail.com>
2022-05-15 16:25:54 -0500

    poco a poco aprendiendo

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+const PageNotFound=()=>{
+    return(
+    <>
+    <div>
+        <h1>Pagina No encontrada </h1>
+        <p>Error 404</p>
+    </div>
+    </>
+    )
+}
+
+export default PageNotFound;
