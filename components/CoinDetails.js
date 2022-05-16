9a803311c4c1d95a00dedd3ecd9366bb1664aec8
andres <echeverry11298@gmail.com>
2022-05-16 16:57:23 -0500

    agregando archivos olvidados

---
+++ components/CoinDetails.js
@@ -0,0 +1,23 @@
+function CoinDetails(props) {
+    return (
+        <div>
+            <div>
+                <h1>{props.name}</h1>
+                <div>
+                    <img src={props.img}/>
+                </div>
+            </div>
+            <div>
+                <ul>
+                    <li>Precio: {props.price}</li>
+                    <li>Simbolo: {props.symbol}</li>
+                    <li>Posicion: {props.rank}</li>
+                    <li>Volumen: {props.volume}</li>
+                    <li>Sitio Web: <a href={props.web} target="_blank">{props.web}</a></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default CoinDetails;
